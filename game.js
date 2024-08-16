4c4e0ea0a76cd898e88e7f7690917d07e5bc03a9
Jonathan Kennel <jonkennel56@gmail.com>
2024-08-16 01:10:37 -0700

    Add files via upload

---
+++ game.js
@@ -0,0 +1,32 @@
+const textElement = document.querySelector("#text");
+const optionButtonsElement = document.querySelector("#option-buttons");
+
+function startGame() {
+    showTextNode(1);
+}
+
+function showTextNode(textNodeIndex) {
+    
+}
+
+function selectOption(option) {
+
+}
+
+const textNodes = [
+    {
+        id: 1,
+        text: "You wake up on the floor of a bathroom.",
+        options: [
+            {
+                text: "Look around"   
+            },
+            {
+                text: "Stand up"
+            }
+        ]
+    }
+];
+
+
+startGame();
